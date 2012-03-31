5c9f2edd5ffa103de402e753820e22059b0d307f
avenj <avenj@cobaltirc.org>
2012-03-31 12:30:07 -0400

    Rework to use a more sensible configuration.
    	modified:   lib/Cobalt/Plugin/RSS.pm
    	modified:   lib/Cobalt/PluginConfs/rss.conf

--- lib/Cobalt/Plugin/RSS.pm
+++ lib/Cobalt/Plugin/RSS.pm
@@ -1,11 +1,14 @@
 package Cobalt::Plugin::RSS;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
-use POE qw/Component::RSSAggregator/;
 use Cobalt::Common;
+
 use File::Spec;
 
+use POE qw/Component::RSSAggregator/;
+
 sub new { bless {}, shift }
+
 sub core { 
   my ($self, $core) = @_;
   return $self->{CORE} = $core if $core and ref $core;
@@ -16,7 +19,73 @@ sub Cobalt_register {
   my ($self, $core) = splice @_, 0, 2;
   $self->core($core);
   
-  $self->{FEEDS} = {};
+  $self->{ANNOUNCE} = {};
+  $self->{FEEDS}    = {};
+  
+  my $pcfg = $core->get_plugin_cfg($self);
+
+  my $feeds = $pcfg->{Feeds};
+  
+  if ($feeds && ref $feeds eq 'HASH') {
+    FEED: for my $feedname (keys %$feeds) {
+    
+      my $uri = $feeds->{$feedname}->{URL};
+      unless ($uri) {
+        $core->log->warn(
+          "Could not add feed $feedname: missing URL directive.",
+          "Check your configuration."
+        );
+        next FEED
+      }
+    
+      my $sendto = $feeds->{$feedname}->{AnnounceTo};
+      unless ($sendto && ref $sendto eq 'HASH') {
+        $core->log->warn(
+          "Could not add feed $feedname: invalid AnnounceTo directive.",
+          "Check your configuration."
+        );
+        next FEED      
+      }
+      
+      my $delay = $feeds->{$feedname}->{Delay} || 180;
+      ## FEEDS{$feedname} = {
+      ##   url   => ...
+      ##   delay => ...
+      ##   HasRun => BOOL
+      ## }
+      $self->{FEEDS}->{$feedname} = {
+        HasRun => 0,
+        url => $uri,
+        delay => $delay,
+      };
+      
+      ## sets up a hash mapping:
+      ##  ANNOUNCE{$feedname} = { $context => [ @channels ], }     
+      CONTEXT: for my $context (keys %$sendto) {
+        unless (ref $sendto->{$context} eq 'ARRAY') {
+          $core->log->warn(
+            "Configured AnnounceTo Context $context is not a list.",
+            "Check your configuration."
+          );
+          next CONTEXT
+        }
+        my $count = @{ $sendto->{$context} };
+        $core->log->debug(
+          "Announcing feed $feedname to $count channels on $context"
+        );
+        push(@{ $self->{ANNOUNCE}->{$feedname}->{$context} },
+          @{ $sendto->{$context} }
+        );
+      }
+    
+    }
+  } else {
+    $core->log->warn(
+      "There are no RSS feeds configured; doing nothing.",
+      "You may want to inspect this plugin's Config: file.",
+      "For an example, see: perldoc Cobalt::Plugin::RSS"
+    );
+  }
 
   $core->log->debug("Spawning session to handle RSS");
   POE::Session->create(
@@ -28,52 +97,16 @@ sub Cobalt_register {
     ],
   );
   
-  my $pcfg = $core->get_plugin_cfg($self);
+  unless ($self->{RSSAGG}) {
+    $core->log->emerg("No RSSAggregator instance?");
+    croak "Could not initialize RSSAggregator"
+  }
 
-  ## pre-configured feeds:
-  ##   Name: URL
-  my $preconf = $pcfg->{Feeds};
-  if ($preconf && ref $preconf eq 'HASH') {
-    for my $feedname (keys %$preconf) {
-      my $uri = $preconf->{$feedname};
-      $self->{FEEDS}->{$uri} = $feedname;
-      $rssagg->add_feed( {
-          name => $feedname, ## FIXME not guaranteed unique
-          delay => 180,
-          url => $uri,
-        },
-      );
-      $core->log->debug("Added configured feed: $feedname ($uri)")
-        if $core->debug > 1;
-    }
-  }
-  
-  my $sendto = $pcfg->{AnnounceTo};
-  unless ($sendto && ref $sendto eq 'HASH') {
-    $core->log->warn("Missing Channels: directive");
-    $core->log->warn(
-      "There are no contexts/channels configured.",
-      "RSS output won't go anywhere...",
-    );
-    $self->{SendTo} = {};
-  } else {
-    ## Context (HASH) -> [ list of channels ]
-    CONTEXT: for my $context (keys %$sendto) {
-      next CONTEXT unless ref $context eq 'ARRAY'
-                          and @$context;
-      
-      CHAN: for my $channel (@$context) {
-        push( @{$self->{SendTo}->{$context}}, $channel );
-        $core->log->debug("Added $channel to $context");
-      }
-    }
-  }
-    
   $core->log->info("Loaded - $VERSION");
   $core->plugin_register( $self, 'SERVER',
     [
-      'public_cmd_rssaddfeed',
-      'public_cmd_rssdelfeed',
+#      'public_cmd_rssaddfeed',
+#      'public_cmd_rssdelfeed',
     ],
   );
   return PLUGIN_EAT_NONE
@@ -81,65 +114,9 @@ sub Cobalt_register {
 
 sub Cobalt_unregister {
   my ($self, $core) = splice @_, 0, 2;
-  ## shutdown component session  FIXME
+  $self->{RSSAGG}->shutdown;
   $core->log->info("Unloaded");
   return PLUGIN_EAT_NONE
-}
-
-sub Bot_public_cmd_rssaddfeed {
-  my ($self, $core) = splice @_, 0, 2;
-  my $context = ${ $_[0] };
-  my $msg     = ${ $_[1] };
-  
-  my $nick = $msg->{src_nick};
-  my $chan = $msg->{channel};
-  
-  my $auth_lev = $core->auth_level($context, $nick);
-
-  my $pcfg = $core->get_plugin_cfg($self);
-  my $minlev = $pcfg->{LevelRequired} // 3;
-
-  return PLUGIN_EAT_NONE
-    unless $core->auth_level($context, $nick) >= $minlev;
-  
-  my $auth_usr = $core->auth_username($context, $nick);
-  
-  my @args = @{ $msg->{message_array} };
-  
-  my ($feedname, $feedurl) = @args;
-  unless ($feedname && $feedurl) {
-    $core->send_event( 'send_message', $context, $chan,
-      "A feed name and URL must be specified."
-    );
-    return PLUGIN_EAT_ALL
-  }
-
-  if ($self->{FEEDS}->{$feedurl}) {
-    $core->send_event( 'send_message', $context, $chan,
-      "Already tracking that feed."
-    );
-    return PLUGIN_EAT_ALL
-  }
-
-  my $rssagg = $self->{RSSAGG};
-  $rssagg->add_feed( {
-      name => $feedname, ## FIXME not guaranteed unique
-      delay => 180,
-      url => $feedurl,
-    },
-  );
-
-  $self->{FEEDS}->{$feedurl} = $feedname;
-
-  $core->send_event( 'send_message', $context, $chan,
-    "Added new feed $feedname for $feedurl",
-  );
-  
-  return PLUGIN_EAT_ALL 
-}
-
-sub Bot_public_cmd_rssdelfeed {
-
 }
 
 
@@ -155,27 +132,55 @@ sub _start {
     callback => $session->postback("_feed"),
     tmpdir => File::Spec->tmpdir(),
   );
-  
+
+  my $feeds = $self->{FEEDS};
+  for my $feedname (keys %$feeds) {
+    my $thisfeed = $feeds->{$feedname};
+    $kernel->post($poe_alias, 'add_feed', 
+      {
+        name  => $feedname,
+        delay => $thisfeed->{delay},
+        url   => $thisfeed->{url},
+      },
+    );
+  }
 }
 
 sub _feed {
   my ($self, $kernel, $session) = @_[OBJECT, KERNEL, SESSION];
   my $core = $self->core;
-  my $feed = $_[ARG1]->[0];
+
+  my $feed  = $_[ARG1]->[0];
   my $title = $feed->title;
+  my $name  = $feed->name;
+  
+  my $feedmeta = $self->{FEEDS}->{$name};
+  my $sendto   = $self->{ANNOUNCE}->{$name};
+  
+  unless ($feedmeta->{HasRun}) {
+    ++$feedmeta->{HasRun};
+    $feed->init_headlines_seen;
+    return
+  }
+  
   HEAD: for my $headline ( $feed->late_breaking_news ) {
     my $this_line = $headline->headline;
-    my $this_headline = "$this_line ($title)";
-    
-    CONTEXT: for my $context ( keys %{ $self->{SendTo} } ) {
-      CHAN: for my $chan ( @$context ) {
+    my $this_headline = "$name: $this_line";
+
+    CONTEXT: for my $context (keys %$sendto) {
+      my $irc = $core->get_irc_obj($context) || next CONTEXT;
+      
+      CHAN: for my $chan ( @{ $sendto->{$context} } ) {
         $core->log->debug("dispatching to $chan ($context)")
           if $core->debug > 1;
+        ## FIXME next CHAN if not present on chan
         $core->send_event( 'send_message', $context, $chan,
           color('bold', "RSS:")." $this_headline",
         );
       } ## CHAN
+    
     } ## CONTEXT
+    
   } ## HEAD
 }
 
